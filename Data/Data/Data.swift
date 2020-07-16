bb446ad4032ef4dbf7c5f750e76f92c026060dad
Jordan Capa <jcapasix@gmail.com>
2020-07-16 03:03:32 -0400

    project structure created

---
+++ Data/Data/Data.swift
@@ -0,0 +1,11 @@
+//
+//  Data.swift
+//  Data
+//
+//  Created by Jordan Capa on 7/16/20.
+//  Copyright © 2020 Jordan Capa. All rights reserved.
+//
+
+class Data {
+
+}
---
+++ Domain/Domain/Domain.swift
@@ -0,0 +1,11 @@
+//
+//  Domain.swift
+//  Domain
+//
+//  Created by Jordan Capa on 7/16/20.
+//  Copyright © 2020 Jordan Capa. All rights reserved.
+//
+
+class Domain {
+
+}
